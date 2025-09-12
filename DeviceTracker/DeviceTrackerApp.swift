c47585097a99c559f1a7ab4c33968e9cfea112d6
Jose Castro <chepe.castro2312@gmail.com>
2025-09-12 08:14:01 -0700

    initial commit

---
+++ DeviceTracker/DeviceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeviceTrackerApp.swift
+//  DeviceTracker
+//
+//  Created by Jose Castro on 9/12/25.
+//
+
+import SwiftUI
+
+@main
+struct DeviceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DeviceTrackerTests/DeviceTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  DeviceTrackerTests.swift
+//  DeviceTrackerTests
+//
+//  Created by Jose Castro on 9/12/25.
+//
+
+import Testing
+@testable import DeviceTracker
+
+struct DeviceTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
